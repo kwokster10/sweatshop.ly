f1581609f984f07968353c20f257ce8f198e046b
kwokster10 <kwokster10@hotmail.com>
2015-05-13 15:55:43 -0400

    clean

--- server.rb
+++ server.rb
@@ -3,7 +3,6 @@ require 'pry'
 
 require_relative 'models/shirt'
 require_relative 'models/buyer'
-require_relative 'lib/connection'
 
 
 #redirect from / to show allshirts pg
@@ -21,7 +20,6 @@ end
 get '/shirts/:id' do
 	id = params[:id].to_i
 	shirt = Shirt.find(id)
-	puts "hello"
 	erb :show, locals: {shirt: shirt}
 end
 
@@ -44,9 +42,9 @@ put '/shirts/:id' do
 	id = params[:id].to_i
 	shirt = Shirt.find(id)
 	editShirt = {
-		name: params[:editName],
-		image: params[:editImage],
-		quantity: params[:editQuantity]
+		name: params[:editName].chomp,
+		image: params[:editImage].chomp,
+		quantity: params[:editQuantity].to_i
 	}
 	shirt.update(editShirt)
 	redirect('/admin')
@@ -55,8 +53,8 @@ end
 #create a shirt
 post "/shirts" do 
 	id = params[:id].to_i
-	name = params[:name]
-	image = params[:image]
+	name = params[:name].chomp
+	image = params[:image].chomp
 	quantity = params[:quantity].to_i
 	Shirt.create(name: name, image: image, quantity: quantity)
 	redirect("/shirts")
@@ -72,16 +70,13 @@ end
 #obtain all shirts by email
 
 get '/admin/purchases' do
-
   buyers = Buyer.all
   shirts = Shirt.all
 
   emails = []
 
   buyers.each do |buyer|
-
     emails.push(buyer.email)
-
   end
 
   emails.uniq!
