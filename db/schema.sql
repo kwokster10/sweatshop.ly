d89fa55ecab4b0feb02e11760d8cc6cd121af6cc
kwokster10 <kwokster10@hotmail.com>
2015-05-13 16:08:43 -0400

    changed schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS shirts (
 	id INTEGER PRIMARY KEY,
 	name TEXT, 
-  image TEXT,
-	quantity INTEGER
+  	image TEXT,
+	quantity INTEGER,
+	price TEXT
 );
 
 CREATE TABLE IF NOT EXISTS buyers (
 	id INTEGER PRIMARY KEY,
 	email TEXT, 
-  p_quantity INTEGER,
-  s_id INTEGER
+  	p_quantity INTEGER,
+ 	s_id INTEGER
 );
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,19 +3,22 @@ require_relative '../models/shirt'
 Shirt.create({
 	name: "Frequent Flyer Tee",
 	image: "http://cdn.karmaloopassets.com/vendor458x698/529200-1.jpg",
-	quantity: 12
+	quantity: 12,
+	price: "19.99"
 	});
 
 Shirt.create({
 	name: "LQR Currency",
 	image: "http://cdn.karmaloopassets.com/vendor458x698/539561-1.jpg",
-	quantity: 12
+	quantity: 12,
+	price: "22.95"
 	});
 
 Shirt.create({
 	name: "Pinapple Express",
 	image: "http://cdn.karmaloopassets.com/vendor458x698/542554-1.jpg",
-	quantity: 12
+	quantity: 12,
+	price: "12.99"
 	});
 
 
