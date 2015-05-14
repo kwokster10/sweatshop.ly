4d80619d7536ef84a99075c3a9017a20ff72e448
Eric Moskowitz <ericmoskowitz@Erics-MacBook-Pro.local>
2015-05-14 09:28:59 -0400

    greater_than_or_equal_to numericality check

--- models/shirt.rb
+++ models/shirt.rb
@@ -3,6 +3,6 @@ require_relative "../lib/connection"
 class Shirt < ActiveRecord::Base
 	validates_presence_of :name, message: "What's the shirt name?"
 	validates_presence_of :image, message: "Don't forget to supply a fancy picture!"
-	validates_numericality_of :quantity, greater_than: 0, message: "How many do you have in stock?"
+	validates_numericality_of :quantity, greater_than_or_equal_to: 0, message: "How many do you have in stock?"
 	validates_presence_of :price, message: "How much does this cost?"
 end
